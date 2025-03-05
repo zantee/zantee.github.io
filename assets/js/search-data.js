594039d91365a31b49445fc6c27224c7570ea23a
zantee <92669216+zantee@users.noreply.github.com>
2025-03-05 09:05:15 +0000

    Deploying to gh-pages from @ zantee/zantee.github.io@fa073a9f2e494eed47c13758ec63ccb7bc5725e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -101,14 +101,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%74%69%61%6E%79%75%7A%68%61%6E%67.%7A%61%6E%74%65%65@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -122,14 +115,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=sXtCHrsAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
